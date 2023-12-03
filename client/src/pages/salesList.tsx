625a5d70fcce6ba072ffb2e6db09d97f02f6e42e
anacolell <anacolell8@gmail.com>
2023-12-03 15:11:04 +0200

    Adds sorting in column headers

--- client/src/pages/salesList.tsx
+++ client/src/pages/salesList.tsx
@@ -2,12 +2,21 @@ import { useEffect, useState } from "react";
 import SaleCard from "../components/saleCard/saleCard";
 import {
   CircularProgress,
+  Drawer,
   Grid,
+  IconButton,
   InputAdornment,
   TextField,
 } from "@mui/material";
 import styles from "./salesList.module.css";
-import { ArrowBackIos, Search } from "@mui/icons-material";
+import {
+  ArrowBackIos,
+  ArrowDropDown,
+  ArrowDropUp,
+  Close,
+  Search,
+  Tune,
+} from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import { getSales } from "../api/getSales";
 
@@ -40,6 +49,12 @@ export default function SalesList() {
   const [sales, setSales] = useState<Sale[]>([]);
   const [loading, setLoading] = useState(true);
   const [searchInput, setSearchInput] = useState("");
+  const [isFiltersDrawerOpen, setIsFiltersDrawerOpen] = useState(false);
+  const [numSalesDisplayed, setNumSalesDisplayed] = useState(10);
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
+  const [sortField, setSortField] = useState<
+    "date" | "product" | "comment" | "total"
+  >("date");
 
   useEffect(() => {
     async function fetchSales() {
@@ -54,6 +69,43 @@ export default function SalesList() {
     return sale.comment.toLowerCase().includes(searchInput.toLowerCase());
   });
 
+  const sortedSales = [...filteredSales].sort((a, b) => {
+    const aValue = a[sortField as keyof Sale];
+    const bValue = b[sortField as keyof Sale];
+
+    if (sortOrder === "asc") {
+      return aValue > bValue ? 1 : -1;
+    } else {
+      return aValue < bValue ? 1 : -1;
+    }
+  });
+
+  const filteredSalesShown = sortedSales.slice(0, numSalesDisplayed);
+
+  function handleFiltersDrawerOpen() {
+    setIsFiltersDrawerOpen(true);
+  }
+
+  function handleFiltersDrawerClose() {
+    setIsFiltersDrawerOpen(false);
+  }
+
+  function handleSort(field: "date" | "product" | "total" | "comment") {
+    if (field === sortField) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortField(field);
+      setSortOrder("asc");
+    }
+  }
+
+  const getArrowIcon = (field: "date" | "product" | "total" | "comment") => {
+    if (field === sortField) {
+      return sortOrder === "asc" ? <ArrowDropUp /> : <ArrowDropDown />;
+    }
+    return null;
+  };
+
   return (
     <main className={styles.pageWrapper}>
       <Link className={styles.goBackWrapper} to="/">
@@ -65,57 +117,128 @@ export default function SalesList() {
         ) : sales.length <= 0 ? (
           <p>Δεν υπάρχουν ακόμη πωλήσεις</p>
         ) : (
-          <Grid container>
-            <Grid
-              item
-              xs={12}
-              textAlign={"right"}
-              sx={{ marginBottom: "24px" }}
-            >
-              <TextField
-                onChange={(e) => setSearchInput(e.target.value)}
-                value={searchInput}
-                size="small"
-                className={styles.textfield}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Search />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid container className={styles.list}>
-              <Grid container className={styles.tableHead}>
-                <Grid item xs={2}>
-                  <p>Ημερομηνία</p>
-                </Grid>
-                <Grid item xs={2}>
-                  <p>Ώρα</p>
-                </Grid>
-                <Grid item xs={2}>
-                  <p>Προϊόν</p>
-                </Grid>
-                <Grid item xs={2}>
-                  <p>Σχόλια</p>
-                </Grid>
-                <Grid item xs={2}>
-                  <p>Σύνολο</p>
-                </Grid>
+          <>
+            <Grid container>
+              <Grid
+                item
+                xs={12}
+                textAlign={"right"}
+                sx={{ marginBottom: "24px" }}
+              >
+                <TextField
+                  onChange={(e) => setSearchInput(e.target.value)}
+                  value={searchInput}
+                  size="small"
+                  className={styles.textfield}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <Search />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                <IconButton
+                  className={styles.filtersBtn}
+                  onClick={handleFiltersDrawerOpen}
+                  color="primary"
+                  sx={{
+                    backgroundColor: "#5384c1",
+                    color: "#fff",
+                    borderRadius: "8px",
+                    marginLeft: "8px",
+                    "&:hover": {
+                      backgroundColor: "#335c8e",
+                    },
+                  }}
+                >
+                  <Tune />
+                </IconButton>
               </Grid>
-              {filteredSales?.map((sale: Sale) => (
-                <Grid item xs={12} key={sale._id}>
-                  <SaleCard
-                    sale={sale}
-                    sales={sales}
-                    setSales={setSales}
-                    key={sale._id}
-                  />
+              <Grid container className={styles.list}>
+                <Grid container className={styles.tableHead}>
+                  <Grid
+                    item
+                    xs={2}
+                    onClick={() => handleSort("date")}
+                    className={styles.columnHeaderWrapper}
+                  >
+                    <div className={styles.columnHeaderWrapper}>
+                      <p>Ημερομηνία</p>
+                      {getArrowIcon("date")}
+                    </div>
+                  </Grid>
+                  <Grid item xs={2} className={styles.columnHeaderWrapper}>
+                    <p>Ώρα</p>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={2}
+                    onClick={() => handleSort("product")}
+                    className={styles.columnHeaderWrapper}
+                  >
+                    <div className={styles.columnHeaderWrapper}>
+                      <p>Προϊόν</p>
+                      {getArrowIcon("product")}
+                    </div>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={2}
+                    onClick={() => handleSort("comment")}
+                    className={styles.columnHeaderWrapper}
+                  >
+                    <div className={styles.columnHeaderWrapper}>
+                      <p>Σχόλια</p>
+                      {getArrowIcon("comment")}
+                    </div>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={2}
+                    onClick={() => handleSort("total")}
+                    className={styles.columnHeaderWrapper}
+                  >
+                    <div className={styles.columnHeaderWrapper}>
+                      <p>Σύνολο</p>
+                      {getArrowIcon("total")}
+                    </div>
+                  </Grid>
                 </Grid>
-              ))}
+                {filteredSalesShown?.map((sale: Sale) => (
+                  <Grid item xs={12} key={sale._id}>
+                    <SaleCard
+                      sale={sale}
+                      sales={sales}
+                      setSales={setSales}
+                      key={sale._id}
+                    />
+                  </Grid>
+                ))}
+                <button
+                  className={styles.button}
+                  onClick={() => setNumSalesDisplayed(numSalesDisplayed + 10)}
+                >
+                  Δείτε περισσότερα
+                </button>
+              </Grid>
             </Grid>
-          </Grid>
+            <Drawer
+              anchor="right"
+              open={isFiltersDrawerOpen}
+              onClose={handleFiltersDrawerClose}
+              className={styles.drawer}
+            >
+              <div className={styles.closeDrawerWrapper}>
+                <IconButton onClick={handleFiltersDrawerClose}>
+                  <Close />
+                </IconButton>
+              </div>
+              <h3 className={styles.drawerTitle}>Arrangement:</h3>
+              <h3 className={styles.drawerTitle}>Filter by:</h3>
+              <h4 className={styles.drawerSubTitle}>Role</h4>
+            </Drawer>
+          </>
         )}
       </div>
     </main>
